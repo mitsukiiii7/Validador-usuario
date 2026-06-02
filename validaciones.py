435e7dbaf9b2141bea9af296cb57a704abbaa06d
mitsukiiii7 <giandigiacomo256@gmail.com>
2026-06-02 18:36:08 -0300

    Update validaciones.py

--- validaciones.py
+++ validaciones.py
@@ -31,7 +31,6 @@ def validar_usuario(nombre):
         es_numero = (num_ascii >= 48 and num_ascii <= 57)
         es_simbolo = (num_ascii == 95 or num_ascii == 46)
 
-        # Esto ahora está dentro del for (indentado correctamente)
         if es_letra:
             tiene_letra = True
             cant_letras = cant_letras + 1
@@ -45,16 +44,13 @@ def validar_usuario(nombre):
             print("Carácter no permitido.")
             return False
 
-    # Ahora USA las variables para que no estén "oscuras"
     if not tiene_letra:
         print("Debe contener al menos una letra.")
         return False
 
-    # Por ejemplo, mostrar que también se detectaron números y símbolos
     if tiene_numero:
         print("El nombre contiene números.")
     if tiene_simbolo:
         print("El nombre contiene símbolos.")
 
-    # Si quieres, puedes usarlos para decidir una categoría más adelante
     return True
